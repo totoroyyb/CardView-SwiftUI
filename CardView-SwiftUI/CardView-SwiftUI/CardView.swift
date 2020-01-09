14bd8929733285b43f8dc33d4ae55ed8e13f1c64
TotoroQ <totoroq@outlook.com>
2020-01-08 21:10:28 -0800

    Update CardView.swift
    Fix safeArea error when expand the card view

--- CardView-SwiftUI/CardView-SwiftUI/CardView.swift
+++ CardView-SwiftUI/CardView-SwiftUI/CardView.swift
@@ -30,17 +30,17 @@ struct CardView: View {
                     withAnimation(.interpolatingSpring(mass: 1, stiffness: 90, damping: 15, initialVelocity: 1)) {
                         self.isShowDetail.toggle()
                         self.control.anyTriggered.toggle()
-                    }
-                    
-                }
-                .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-                .offset(x: self.isShowDetail ? -geo.frame(in: .global).minX : 0, y: self.isShowDetail ? -geo.frame(in: .global).minY : 0)
-                .frame(height: self.isShowDetail ? UIScreen.main.bounds.height : nil)
-                .frame(width: self.isShowDetail ? UIScreen.main.bounds.width : nil)
+                }    
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .offset(x: self.isShowDetail ? -geo.frame(in: .global).minX : 0, y: self.isShowDetail ? -geo.frame(in: .global).minY : 0)
+            .frame(height: self.isShowDetail ? UIScreen.main.bounds.height : nil)
+            .frame(width: self.isShowDetail ? UIScreen.main.bounds.width : nil)
         }
         .frame(width: UIScreen.main.bounds.width - 40)
         .frame(height: 300)
         .offset(x: control.anyTriggered && !isShowDetail ? UIScreen.main.bounds.width : 0)
+        .opacity(control.anyTriggered && !isShowDetail ? 0 : 1)
     }
 }
 
@@ -50,10 +50,12 @@ struct CardView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             CardView(subtitle: "MEET THE DEVELOPER", title: "Insider VSCO's Imaging Lab", backgroundImage: Image("bg1"), briefSummary: "How VSCO brings analog authenticity to your digital shots", description: desPlaceholer, isShowDetail: false)
+            .environmentObject(CardView_Control())
             
-            CardView(subtitle: "MEET THE DEVELOPER", title: "Insider VSCO's Imaging Lab", backgroundImage: Image("bg1"), briefSummary: "How VSCO brings analog authenticity to your digital shots", description: desPlaceholer, isShowDetail: true)
-            
-            TopView(subtitle: "MEET THE DEVELOPER", title: "Insider VSCO's Imaging Lab", backgroundImage: Image("bg1"), briefSummary: "How VSCO brings analog authenticity to your digital shots")
+//            CardView(subtitle: "MEET THE DEVELOPER", title: "Insider VSCO's Imaging Lab", backgroundImage: Image("bg1"), briefSummary: "How VSCO brings analog authenticity to your digital shots", description: desPlaceholer, isShowDetail: true)
+//
+//            TopView(subtitle: "MEET THE DEVELOPER", title: "Insider VSCO's Imaging Lab", backgroundImage: Image("bg1"), briefSummary: "How VSCO brings analog authenticity to your digital shots")
+//                .background(Color.black)
         }
     }
 }
@@ -67,11 +69,12 @@ struct CardInnerView: View {
     var description: String
     
     @Binding var isShow: Bool
+    @State private var translation = CGSize.zero
     
     var body: some View {
         GeometryReader { geo in
-            ScrollView {
-                TopView(subtitle: self.subtitle, title: self.title, backgroundImage: self.backgroundImage, briefSummary: self.briefSummary)
+            VStack {
+                TopView(subtitle: self.subtitle, title: self.title, backgroundImage: self.backgroundImage, briefSummary: self.briefSummary, isShow: self.$isShow)
                     .frame(height: 300)
                     .background(
                         self.backgroundImage
@@ -79,14 +82,15 @@ struct CardInnerView: View {
                             .aspectRatio(contentMode: .fill)
                             .frame(maxWidth: geo.size.width, maxHeight: geo.size.height)
                             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-                )
+                    )
+                    .offset(y: self.translation.height)
                 
-                Text(self.description)
-                    .font(.body)
-                    .foregroundColor(.white)
-                    .padding()
-                    .opacity(self.isShow ? 1 : 0)
-                    .animation(.linear)
+                if self.isShow {
+                    ScrollView {
+                        ExpandableView(description: self.description, isShow: self.$isShow)
+                            .transition(.opacity)
+                    }
+                }
             }
             .background(Color.black)
         }
@@ -99,36 +103,63 @@ struct TopView: View {
     var backgroundImage: Image
     var briefSummary: String
     
+    @Binding var isShow: Bool
+    
     var body: some View {
-        VStack(alignment: .center) {
-            HStack {
-                VStack(alignment: .leading) {
-                    Text(self.subtitle)
-                        .font(.caption)
-                        .foregroundColor(.gray)
-                    
-                    Text(self.title)
-                        .font(.headline)
-                        .foregroundColor(.white)
+        GeometryReader { geo in
+            VStack(alignment: .center, spacing: 0) {
+                if self.isShow {
+                    Rectangle()
+                        .frame(height: UIApplication.shared.windows.first?.safeAreaInsets.top)
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(Color.clear)
+                        .onTapGesture {
+                            print(geo.safeAreaInsets.top)
+                        }
+                }
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(self.subtitle)
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                        
+                        Text(self.title)
+                            .font(.headline)
+                            .foregroundColor(.white)
+                        
+                        Spacer()
+                    }
+                    .lineLimit(3)
                     
                     Spacer()
                 }
-                .lineLimit(3)
+                .padding()
                 
                 Spacer()
+                
+                HStack(alignment: .center) {
+                    Text(self.briefSummary)
+                        .foregroundColor(.white)
+                        .font(.caption)
+                        .lineLimit(3)
+                    Spacer()
+                }
+                .padding()
             }
-            .padding()
-            
-            Spacer()
-            
-            HStack(alignment: .center) {
-                Text(self.briefSummary)
-                    .foregroundColor(.white)
-                    .font(.caption)
-                    .lineLimit(3)
-                Spacer()
-            }
-            .padding()
         }
+    }
+}
+
+struct ExpandableView: View {
+    var description: String
+    
+    @Binding var isShow: Bool
+    
+    var body: some View {
+        Text(self.description)
+            .font(.body)
+            .foregroundColor(.white)
+            .padding()
     }
 }
